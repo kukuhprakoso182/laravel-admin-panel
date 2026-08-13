61215884f45efd4e30eb9259fd162db57fa790cc
kukuh prakoso <kukuhprakoso182@gmail.com>
2026-08-13 15:06:02 +0700

    feat(format-utils): add normalizeOptions

--- resources/js/utils/format.js
+++ resources/js/utils/format.js
@@ -11,5 +11,45 @@ window.formatUtils = (function () {
         return new URLSearchParams(params);
     }
 
-    return { formatDate, buildQueryParams };
+    // di dalam formatUtils object/module
+    function formatCurrency(value, currency = 'IDR') {
+        const number = Number(value) || 0;
+
+        if (currency === 'USD') {
+            return new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+            }).format(number);
+        }
+
+        return new Intl.NumberFormat('id-ID', {
+            style: 'currency',
+            currency: 'IDR',
+            minimumFractionDigits: 0,
+        }).format(number);
+    }
+
+    /**
+     * Menyeragamkan berbagai bentuk option (mis. {id, name}, {alat_id, nama_alat},
+     * {value, label}) jadi selalu punya key value & label yang dibutuhkan
+     * <x-atoms.select>, tanpa membuang field asli lainnya.
+     *
+     * @param {Array}  list       Array of object dari response API
+     * @param {Array}  valueKeys  Kandidat nama kolom untuk value, dicoba berurutan
+     * @param {Array}  labelKeys  Kandidat nama kolom untuk label, dicoba berurutan
+     */
+    function normalizeOptions(list, valueKeys = ['value', 'id'], labelKeys = ['label', 'name']) {
+        return (list || []).map(item => {
+            const valueKey = valueKeys.find(k => item[k] !== undefined);
+            const labelKey = labelKeys.find(k => item[k] !== undefined);
+
+            return {
+                ...item,
+                value: valueKey ? item[valueKey] : null,
+                label: labelKey ? item[labelKey] : '',
+            };
+        });
+    }
+
+    return { formatDate, buildQueryParams, formatCurrency, normalizeOptions  };
 })();
