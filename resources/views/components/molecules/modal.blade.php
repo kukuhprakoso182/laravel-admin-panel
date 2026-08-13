ab444dab9ed5ad0902b4eb2b69efddad2cb1472f
kukuh prakoso <kukuhprakoso182@gmail.com>
2026-08-13 10:36:23 +0700

    feat(modal): support button esc to dismiss

--- resources/views/components/molecules/modal.blade.php
+++ resources/views/components/molecules/modal.blade.php
@@ -34,6 +34,7 @@
 @endphp
 
 <div x-show="{{ $show }}" x-cloak
+    @keydown.escape.window="{{ $show }} = false"
      class="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black/50"
      x-transition.opacity>
     <div
