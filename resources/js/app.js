0d6d6f524c8a99db83387f023d23f73fe082f8f5
kukuh prakoso <kukuhprakoso182@gmail.com>
2026-08-14 23:52:04 +0700

    chore: add chart js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,9 @@ import 'remixicon/fonts/remixicon.css';
 import './utils/utils-register.js';
 import './components/component-register.js';
 import { bootAlpine } from './alpine-loader';
+import Chart from 'chart.js/auto';
 
 Alpine.plugin(collapse);
 window.Alpine = Alpine;
 bootAlpine(Alpine);
+window.Chart = Chart;
